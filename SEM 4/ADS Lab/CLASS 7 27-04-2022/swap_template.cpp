e41d253af66979242e8ebb3f4a24c4e173ed9110
Rahul Prasanth D <rahulprasanth2002@gmail.com>
2022-05-03 11:59:01 +0530

    Edited

--- SEM 4/ADS Lab/CLASS 7 27-04-2022/swap_template.cpp
+++ SEM 4/ADS Lab/CLASS 7 27-04-2022/swap_template.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 template <typename T>
@@ -17,21 +18,25 @@ int main()
     float m=1.1,n=2.2;
     double l=111.111,j=222.2222;
     char a='A',b='B';
+    string p=" Rahul ",q=" Prasanth ";
 
-    cout<<"Before swapping "<<endl;
-    cout<<"x="<<x<<" y="<<y<<endl;
-    cout<<"m="<<m<<" n="<<n<<endl;
-    cout<<"l="<<l<<" j="<<j<<endl;
-    cout<<"a="<<a<<" b="<<b<<endl;
+    cout << "Before swapping " << endl;
+    cout << " x = " << x << " , y =" << y << endl;
+    cout << " m = " << m << " , n =" << n << endl;
+    cout << " l = " << l << " , j =" << j << endl;
+    cout << " a = " << a << " , b =" << b << endl;
+    cout << " p = " << p << " , q =" << q << endl;
 
-    swapping<int>(x,y);
-    swapping<float>(m,n);
-    swapping<double>(l,j);
-    swapping<char>(a,b);
+    swapping<int>(x, y);
+    swapping<float>(m, n);
+    swapping<double>(l, j);
+    swapping<char>(a, b);
+    swapping<string>(p,q);
 
-    cout<<"\nAfter swapping "<<endl;
-    cout<<"x="<<x<<" y="<<y<<endl;
-    cout<<"m="<<m<<" n="<<n<<endl;
-    cout<<"l="<<l<<" j="<<j<<endl;
-    cout<<"a="<<a<<" b="<<b<<endl;
+    cout << "\nAfter swapping " << endl;
+    cout << " x = " << x << " , y =" << y << endl;
+    cout << " m = " << m << " , n =" << n << endl;
+    cout << " l = " << l << " , j =" << j << endl;
+    cout << " a = " << a << " , b =" << b << endl;
+    cout << " p = " << p << " , q =" << q << endl;
 }
