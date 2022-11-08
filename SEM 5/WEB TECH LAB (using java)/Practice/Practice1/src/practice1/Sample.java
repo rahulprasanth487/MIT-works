0d2131513ba60d41766dd47a554d3a6e620a3d56
Rahul Prasanth D <rahulprasanth2002@gmail.com>
2022-11-08 23:46:36 +0530

    new

--- SEM 5/WEB TECH LAB (using java)/Practice/Practice1/src/practice1/Sample.java
+++ SEM 5/WEB TECH LAB (using java)/Practice/Practice1/src/practice1/Sample.java
@@ -1,11 +1,10 @@
-import java.net.*;
+import java.util.*;
 import java.io.*;
 
 public class Sample {
 
     public static void main(String[] args) throws IOException{
-        System.out.println("Enter teh name= ");
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(bf.readLine());
+        ArrayList<String> x=new ArrayList<String>();
+        
     }
 }
--- SEM 5/WEB TECH LAB (using java)/SESSION 6 21-09-2022/RegEx.java
+++ SEM 5/WEB TECH LAB (using java)/SESSION 6 21-09-2022/RegEx.java
@@ -1,5 +1,4 @@
 
-
 import java.util.regex.*;
 import java.util.Scanner;
 /**
